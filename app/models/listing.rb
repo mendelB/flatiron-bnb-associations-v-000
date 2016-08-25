2717868dd64770f26e55d796adc6db128517d148
mendelB <mendelblesofsky@gmail.com>
2016-08-25 23:07:39 +0200

    add models and associations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,14 @@
 class Listing < ActiveRecord::Base
-  has_many :reservations
-  has_many :reviews
-  has_many :guests
   belongs_to :neighborhood
-  belongs_to :city
   belongs_to :host, :class_name => "User"
-  has_many :guests, :class_name => "User"
+
+  has_many :reservations
+  has_many :guests, :class_name => "User", through: :reservations
+  has_many :reviews, through: :reservations
+
+  def neighborhood_attributes=(attrs)
+      unless attrs[:name].empty?
+        self.neighborhood = Neighborhood.find_or_create(attrs)
+      end
+  end
 end
